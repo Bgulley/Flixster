9d9496304b69f35a825fd1da7da7c2f7daa46a40
Brittney Gulley <Bgull>
2019-10-24 13:09:33 -0500

    added gridview

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by cs_apple_05 on 10/23/19.
+//  Copyright © 2019 BrittneyGulley94@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
